3b957b5c259c3bc06192cea2fa56f13c22bf5f86
Nam Dinh <nam.dt247@gmail.com>
2021-03-10 16:31:38 +0700

    function statistic

---
+++ StudentManagement/Controllers/StatisticController.cs
@@ -0,0 +1,60 @@
+﻿using StudentManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagement.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Statistic
+        public ActionResult Index()
+        {
+
+            return View(db.Sessions.ToList());
+        }
+
+        public ActionResult GetDetailSession(int Id)
+        {
+            if (Id == null)
+            {
+                return Redirect("Index");
+            }
+            var ListSessionDetail = db.SessionDetails.Where(x => x.SessionDetailId == Id).ToList();
+            var numberSessionComplete = db.SessionDetails.Where(x => x.Status == SessionDetail.SessionDetailStatus.DONE).ToList().Count;
+            var session = db.Sessions.Find(Id);
+            ViewData["SubjectName"] = "";
+            ViewData["numberSession"] = 0;
+            ViewData["numberStudent"] = 0;
+            var lstStudent = new List<ApplicationUser>();
+            if (session != null)
+            {
+                var subject = db.Subjects.Find(session.SubjectId);
+                ViewData["SubjectName"] = subject.SubjectName;
+                ViewData["numberSession"] = session.NumBerSession;
+                var ListStudent = session.ListStudent.Split(',').ToList();
+                ViewData["numberStudent"] = ListStudent.Count;
+                foreach(var userId in ListStudent)
+                { 
+                    var student = db.Users.Find(userId);
+                    var numberAttend = db.Attendances.Where(x => x.ApplicationUserId == userId && x.Attend == 1).ToList().Count;
+                    
+                    if (student != null)
+                    {
+                        lstStudent.Add(student);
+                    }
+                }
+            }
+            ViewData["ListSessionDetail"] = ListSessionDetail;
+            ViewData["session"] = session;
+            ViewData["numberSessionComplete"] = numberSessionComplete;
+            ViewData["ListStudent"] = lstStudent;
+           
+            return View();
+        }
+    }
+}
--- StudentManagement/Models/IdentityModels.cs
+++ StudentManagement/Models/IdentityModels.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -42,6 +43,16 @@ namespace StudentManagement.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
+        }
+
+        public int GetNumberAttend(string UserId)
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                var numberAttend = 
+                    db.Attendances.Where(x => x.ApplicationUserId == UserId && x.Attend == 1).ToList().Count;
+                return numberAttend;
+            }
         }
     }
 
