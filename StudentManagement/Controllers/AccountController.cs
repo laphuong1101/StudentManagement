2a0ac19c676661d4a488ba58b101efc9f5a9e10a
Nam Dinh <nam.dt247@gmail.com>
2021-03-11 10:20:39 +0700

    update function logout

--- StudentManagement/Controllers/AccountController.cs
+++ StudentManagement/Controllers/AccountController.cs
@@ -387,14 +387,11 @@ namespace StudentManagement.Controllers
             return View(model);
         }
 
-        //
-        // POST: /Account/LogOff
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        [HttpGet]
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
